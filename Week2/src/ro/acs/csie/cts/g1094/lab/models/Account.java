e18fd43337445122d12516caa3917571093f277c
Pau1803 <petcupaula18@stud.ase.ro>
2021-03-02 15:26:53 +0200

    Week2 final

--- Week2/src/ro/acs/csie/cts/g1094/lab/models/Account.java
+++ Week2/src/ro/acs/csie/cts/g1094/lab/models/Account.java
@@ -1,8 +1,11 @@
 package ro.acs.csie.cts.g1094.lab.models;
 
+import ro.acs.csie.cts.g1094.lab.exception.IllegalTransferException;
+import ro.acs.csie.cts.g1094.lab.exception.InsufficientFundsException;
+
 public abstract class Account {
 	public abstract void deposit(Double amount);
-	public abstract void withdraw(Double amount);
+	public abstract void withdraw(Double amount) throws InsufficientFundsException;
 	public abstract  Object getBalance();
-	public abstract void transfer(Account destination, Double amount);
+	public abstract void transfer(Account destination, Double amount) throws InsufficientFundsException, IllegalTransferException;
 }
---
+++ Week2/src/ro/acs/csie/cts/g1094/lab/models/CurrentAccount.java
@@ -0,0 +1,40 @@
+package ro.acs.csie.cts.g1094.lab.models;
+
+import ro.acs.csie.cts.g1094.lab.exception.IllegalTransferException;
+import ro.acs.csie.cts.g1094.lab.exception.InsufficientFundsException;
+
+public class CurrentAccount extends BankAccount {
+	public static final double MAX_CREDIT = 5000;
+	
+	public CurrentAccount(String iban) {
+		super(iban, CurrentAccount.MAX_CREDIT);
+	}
+	
+	@Override
+	public void deposit(Double amount) {
+		this.balance +=amount;
+	}
+
+	@Override
+	public void withdraw(Double amount) throws InsufficientFundsException {
+		if(amount >this .balance) {
+			throw new InsufficientFundsException("You do not have the requiered amount");
+			
+		}
+		else {
+			this.balance -= amount; 
+		}
+
+	}
+
+	@Override
+	public void transfer(Account destination, Double amount) throws InsufficientFundsException, IllegalTransferException {
+		if(this == destination) {
+			throw new IllegalTransferException();
+		}
+		this.withdraw(amount);
+		destination.deposit(amount);
+
+	}
+
+}
