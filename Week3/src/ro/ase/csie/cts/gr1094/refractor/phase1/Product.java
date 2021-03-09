c19f088ecf9c35c612feb222c53b607bfa10062c
Pau1803 <petcupaula18@stud.ase.ro>
2021-03-09 15:32:55 +0200

    final commit

--- Week3/src/ro/ase/csie/cts/gr1094/refractor/phase1/Product.java
+++ Week3/src/ro/ase/csie/cts/gr1094/refractor/phase1/Product.java
@@ -1,13 +1,22 @@
 package ro.ase.csie.cts.gr1094.refractor.phase1;
 
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeException;
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
+
 public class Product {
 	
 	public static final int MAX_AGE_DISCOUNT =10;
 	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
-	public float computePriceWithDiscount(ProductType productType, float price, int accountAge)
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
 	
 	
 	  {
+		if(price <= 0) {
+			throw new InvalidPriceException();
+		}
+		if(accountAge < 0) {
+			throw new InvalidAgeException();
+		}
 	    float finalPrice = 0;
 	    float fidelityDiscount = (accountAge > MAX_AGE_DISCOUNT ) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100;
 	    switch(productType) {
