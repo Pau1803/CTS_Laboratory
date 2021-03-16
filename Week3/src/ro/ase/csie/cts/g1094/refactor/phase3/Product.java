22e6e26298238ac79ad2cb4984ead095f4e8bc8e
Pau1803 <petcupaula18@stud.ase.ro>
2021-03-16 15:34:13 +0200

    FINAL commit

--- Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/Product.java
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/Product.java
@@ -2,63 +2,61 @@ package ro.ase.csie.cts.g1094.refactor.phase3;
 
 import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeException;
 import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
+import ro.ase.csie.cts.g1094.refactor.phase2.ProductType;
+import ro.ase.csie.cts.g1094.refactor.phase3.services.Marketing2021Strategy;
 import ro.ase.csie.cts.g1094.refactor.phase3.services.MarketingServiceInterface;
-import ro.ase.csie.cts.gr1094.refractor.phase1.ProductType;
+import ro.ase.csie.cts.g1094.refactor.phase3.services.ValidatorServiceInterface;
 
 public class Product {
 	
-	MarketingServiceInterface mkService=null;
+	MarketingServiceInterface mkService = null;
+	ValidatorServiceInterface validator = null;
 	
-	public Product(MarketingServiceInterface mkService) {
-		//if(mkService == null) {
-			//throw new NullPointerException();
-		//}
-		//this.mkService = mkService;
+	public Product(MarketingServiceInterface mkService, ValidatorServiceInterface validator) {
+		
+		this.setMarketingService(mkService);
+		this.validator = validator;
 	}
 	
 	//optional - based on design specs
-	public void setMarketingService(MarketingServiceInterface mkService) {
+	public void setMarketingService(MarketingServiceInterface mkService)
+	{
 		if(mkService == null) {
 			throw new NullPointerException();
 		}
 		this.mkService = mkService;
+		
 	}
-	public static float getDiscountValue(float price, float discount) {
-		return discount * price;
+	
+	public static float getDiscountValue(float price,float discount) {
+		return discount*price;
 	}
 	
 	private static float getPriceWithDiscountAndFidelity(float price, float discountValue, float fidelityDiscount) {
-		return (price - discountValue)  * (1 - fidelityDiscount);
+		return (price -discountValue )  * (1 - fidelityDiscount);
 	}
 	
 	
 	
-	private static float getFinalPrice(float price,
-			float fidelityDiscount, ProductType type) {
-		float discountValue = getDiscountValue(price, type.getDiscount());
-    	float finalPrice=getPriceWithDiscountAndFidelity(price, discountValue, fidelityDiscount);
-    	return finalPrice;
+	private static float getFinalPrice (float price, float fidelityDiscount, ProductType type) {
+		float discountValue= getDiscountValue(price, type.getDiscount());
+	    float finalPrice = getPriceWithDiscountAndFidelity(price, discountValue, fidelityDiscount);
+	    return finalPrice;
 	}
 	
-	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidAgeException, InvalidPriceException
 	  {
-		if(price<=0) {
-			throw new InvalidPriceException();
-		}
-		if(accountAge<0) {
-			throw new InvalidAgeException();
-		}
 		
-	    float finalPrice = 0;
-	    float fidelityDiscount = (productType== ProductType.NEW) ? 0 : mkService.getFidelityDiscount(accountAge);
-	    float discountValue=0;
-	   
-	    finalPrice= getFinalPrice(price,fidelityDiscount,productType);
+		validator.validatePrice(price);
+		validator.validateAge(accountAge);
 	    
+	    float fidelityDiscount =(productType == ProductType.NEW)? 0 :mkService.getFidelityDiscount(accountAge);
 	    
-	    
+	    float finalPrice = getFinalPrice(price,fidelityDiscount, productType);
 	    
 	    return finalPrice;
+	    
+	 
+	  
 	  }
-
 }
---
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase3/ProductType.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1094.refactor.phase3;
+
+public class ProductType {
+
+	public static final ro.ase.csie.cts.g1094.refactor.phase2.ProductType DISCOUNTED = null;
+
+}
