e5e5e00fa82187e48e3b0eb025c3815e2894b5cf
Pau1803 <petcupaula18@stud.ase.ro>
2021-03-30 14:46:01 +0300

    before break

---
+++ Week6/src/ro/ase/csie/cts/gr1094/dp/builder/Pistol.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.gr1094.dp.builder;
+
+public class Pistol implements WeaponInterface {
+	@Override
+public void pewPew() {
+	System.out.println("Pistol->->->");
+}
+}
---
+++ Week6/src/ro/ase/csie/cts/gr1094/dp/builder/WeaponInterface.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.gr1094.dp.builder;
+
+public interface WeaponInterface {
+	public abstract void pewPew();
+}
