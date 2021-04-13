60cda6c71776796481951f8fef504ff4d9e7b3c6
Pau1803 <petcupaula18@stud.ase.ro>
2021-04-13 15:46:28 +0300

    Facade, Adaptor, Decorator

--- Week8- Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/adapter/disney/DonaldDuck.java
+++ Week8- Facade, Adapter and Decorator/src/ro/ase/csie/cts/g1094/dp/adapter/disney/DonaldDuck.java
@@ -1,12 +1,11 @@
 package ro.ase.csie.cts.g1094.dp.adapter.disney;
 
-public class DonaldDuck implements DisneyActions{
-	
-	public final String name="Donald Duck";
+public class DonaldDuck implements DisneyActions {
+
+	public final String name = "Donald Duck";
 	int powerLevel;
 	
 	
-
 	public DonaldDuck(int powerLevel) {
 		super();
 		this.powerLevel = powerLevel;
@@ -20,16 +19,28 @@ public class DonaldDuck implements DisneyActions{
 
 	@Override
 	public void isWounded(int power) {
-		powerLevel-=power;
-		System.out.println("Power decreased to" + powerLevel);
+		powerLevel -=power;
+		System.out.println("Power decreased to " + powerLevel);
 		
 	}
 
 	@Override
 	public void restorePower(int power) {
-		powerLevel+=power;
-		System.out.println("Power increased to" + powerLevel);
+		powerLevel += power;
+		System.out.println("Power increased to " + powerLevel);
 		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	@Override
+	public int getPower() {
+		// TODO Auto-generated method stub
+		return this.powerLevel;
 	}
 
 }
