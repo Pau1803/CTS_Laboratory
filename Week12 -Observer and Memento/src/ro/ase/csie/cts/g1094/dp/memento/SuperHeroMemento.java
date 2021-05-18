7853ce2caf95bd9d8e0566fb15895a1c4bf94500
Pau1803 <petcupaula18@stud.ase.ro>
2021-05-18 14:56:15 +0300

    *With saved files*

--- Week12 -Observer and Memento/src/ro/ase/csie/cts/g1094/dp/memento/SuperHeroMemento.java
+++ Week12 -Observer and Memento/src/ro/ase/csie/cts/g1094/dp/memento/SuperHeroMemento.java
@@ -1,5 +1,34 @@
 package ro.ase.csie.cts.g1094.dp.memento;
 
-public class SuperHeroMemento {
+import java.sql.Date;
 
+public class SuperHeroMemento {
+	String name;
+	private int lifePoints;
+	String magic;
+	
+	Date timestamp;
+
+	public SuperHeroMemento(String name, int lifePoints, String magic) {
+		super();
+		this.name = name;
+		this.lifePoints = lifePoints;
+		this.magic = magic;
+		this.timestamp = timestamp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getLifePoints() {
+		return lifePoints;
+	}
+
+	public String getMagic() {
+		return magic;
+	}
+
+
+	
 }
